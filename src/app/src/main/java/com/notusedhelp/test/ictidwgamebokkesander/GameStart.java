514a554a3213c772af9b6fca71651f5c878a9791
Bokke <bokke1010@gmail.com>
2018-05-24 15:08:04 +0200

    What?

--- src/app/src/main/java/com/notusedhelp/test/ictidwgamebokkesander/GameStart.java
+++ src/app/src/main/java/com/notusedhelp/test/ictidwgamebokkesander/GameStart.java
@@ -2,12 +2,20 @@ package com.notusedhelp.test.ictidwgamebokkesander;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class GameStart extends AppCompatActivity {
 
+    private Button start_game;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_start);
     }
+}
+
+public class Main extends ActionBarActivity implements View.OnClickListener {
+    // do your stuff
 }
